29a3b4389a85d874c5b3b16153a828cebbcdcaab
Chammika Udalagama <chammika@nus.edu.sg>
2021-08-27 11:05:25 +0800

    Most updated version for Darren and Kai Ze to start work.

--- 02_workshop_02_exploratory-analysis-with-pandas.Rmd
+++
@@ -1,1 +0,0 @@
-# Using Pandas to Explore Data
--- _my_common_setup_info.R
+++ _my_common_setup_info.R
@@ -1,10 +1,28 @@
-counter_exercises <- 0
-counter_exercises_all <- 0
+counter_exercises <- 1
+counter_exercises_all <- 1
+counter_examples <- 1
+counter_examples_all <- 1
 
 my_fun <- list(counter_exercises = 0)
 class(my_fun) <- "My Functions"
 
 # To keep track of the exercises
+my_fun.example <- function(text, reset, same = F){
+   if(!missing(reset)){
+        counter_examples <<- 0
+      }
+
+  if (same){
+
+  }else{
+    counter_examples <<- counter_examples + 1
+    counter_examples_all <<- counter_examples_all + 1
+  }
+
+  return(paste('### <span class = "my-examples">Example ',counter_examples,'| </span>',text,'{.unnumbered .panelset}\n'))
+}
+
+# To keep track of the examples
 my_fun.exercise <- function(text, reset, same = F){
    if(!missing(reset)){
         counter_exercises <<- 0
