a132a64099a6aedb8c3fe7b3b1abf638798a3f76
KazeSps <88024705+KazeSps@users.noreply.github.com>
2021-09-03 16:34:56 +0900

    Added some Exercises
    Two Sample exercises have been done, cleaned up some code that doesn't work

--- files_python/a-simple-plot.py
+++ files_python/a-simple-plot.py
@@ -32,19 +32,3 @@ plt.grid(alpha=.25)
 plt.legend(loc='upper left')
 plt.savefig(f'{argv[0]}.png', dpi=150)  # Hide
 plt.show()
-# %% Exercises
-'''
-- Here we covered how basic plotting works
-- Try generating the following graph with black circle markers and Red line
-'''
-
-
-#X and Y Datasets 
-X=[-1,0,1]
-Y=[1,0,-1]
-#Here we would need to start by using plt.plot(X-array,Y-Array)
-plt.plot(X,Y     )
-plt.plot(X,Y,color='red', marker='o',mfc='black' ) # Hide
-#plt.savefig(f'{argv[0]}.png', dpi=100) # Hide
-#Next we would need to showcase the plot generated.
-plt.show() 
