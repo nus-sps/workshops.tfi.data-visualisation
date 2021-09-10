44fd65541f32e25fa9850984ccef417fe2ef58b8
Chammika Udalagama <chammika@nus.edu.sg>
2021-09-10 16:19:41 +0800

    Commit after a clean

--- build_site.sh
+++ build_site.sh
@@ -3,7 +3,7 @@
 #R -e "Sys.setenv(RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc');rmarkdown::render('how
 #.Rmd',output_file='how.html')"
 echo "-----------------------------------> Knitting site."
-# R -e "rmarkdown::clean_site()"
+#R -e "rmarkdown::clean_site()"
 
 #R -e "Sys.setenv(RSTUDIO_PANDOC='/Applications/RStudio.app/Contents/MacOS/pandoc');rmarkdown::render_site(output_format = 'bookdown::bs4_book', encoding = 'UTF-8')"
 R -e "Sys.setenv(RSTUDIO_PANDOC='/usr/lib/rstudio/bin/pandoc');rmarkdown::render_site(output_format = 'bookdown::bs4_book', encoding = 'UTF-8')"
